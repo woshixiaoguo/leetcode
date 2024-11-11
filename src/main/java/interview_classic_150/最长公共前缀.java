47ae1b00b50817644d45a0d83f2b5588f4b1a7c4
Apollo <isguohr@qq.com>
2024-11-11 12:34:17 +0800

    面试经典150题-数组/字符串

---
+++ src/main/java/interview_classic_150/最长公共前缀.java
@@ -0,0 +1,151 @@
+package interview_classic_150;
+
+import org.junit.Test;
+
+/**
+ * 14 最长公共前缀
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：strs = ["flower","flow","flight"]
+ * 输出："fl"
+ * 示例 2：
+ * 
+ * 输入：strs = ["dog","racecar","car"]
+ * 输出：""
+ * 解释：输入不存在公共前缀。
+ * 
+ * 
+ * 提示：
+ * 
+ * 1 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] 仅由小写英文字母组成
+ */
+
+public class 最长公共前缀 {
+
+    // 纵向扫描
+    public String longestCommonPrefix(String[] strs) {
+        int index = 0;
+        StringBuilder sb = new StringBuilder();
+        outer: while (index < strs[0].length()) {
+            char c = strs[0].charAt(index);
+            for (int i = 1; i < strs.length; i++) {
+                if (index >= strs[i].length() || strs[i].charAt(index) != c) {
+                    break outer;
+                }
+            }
+            sb.append(c);
+            index++;
+        }
+        return sb.toString();
+    }
+
+    // 横向扫描
+    public String longestCommonPrefix2(String[] strs) {
+        if (strs == null || strs.length == 0)
+
+        {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    public String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+    }
+
+    // 分治
+    public String longestCommonPrefix3(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        } else {
+            return longestCommonPrefix(strs, 0, strs.length - 1);
+        }
+    }
+
+    public String longestCommonPrefix(String[] strs, int start, int end) {
+        if (start == end) {
+            return strs[start];
+        } else {
+            int mid = (end - start) / 2 + start;
+            String lcpLeft = longestCommonPrefix(strs, start, mid);
+            String lcpRight = longestCommonPrefix(strs, mid + 1, end);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    public String commonPrefix(String lcpLeft, String lcpRight) {
+        int minLength = Math.min(lcpLeft.length(), lcpRight.length());
+        for (int i = 0; i < minLength; i++) {
+            if (lcpLeft.charAt(i) != lcpRight.charAt(i)) {
+                return lcpLeft.substring(0, i);
+            }
+        }
+        return lcpLeft.substring(0, minLength);
+    }
+
+    // 二分查找
+    public String longestCommonPrefix4(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int minLength = Integer.MAX_VALUE;
+        for (String str : strs) {
+            minLength = Math.min(minLength, str.length());
+        }
+        int low = 0, high = minLength;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (isCommonPrefix(strs, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return strs[0].substring(0, low);
+    }
+
+    public boolean isCommonPrefix(String[] strs, int length) {
+        String str0 = strs[0].substring(0, length);
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            String str = strs[i];
+            for (int j = 0; j < length; j++) {
+                if (str0.charAt(j) != str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void test() {
+        String[] strs = { "flower", "flow", "flight" };
+        String[] strs2 = { "ab", "a" };
+        String ans = longestCommonPrefix(strs2);
+        System.out.println(ans);
+
+    }
+}
