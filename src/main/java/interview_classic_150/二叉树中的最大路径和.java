a6b806d7a82818a683a19d0f546db2f1677039cc
Apollo <isguohr@qq.com>
2024-12-27 13:46:51 +0800

    面试经典150题-二叉树

---
+++ src/main/java/interview_classic_150/二叉树中的最大路径和.java
@@ -0,0 +1,75 @@
+package interview_classic_150;
+
+import org.junit.Test;
+
+import ds.TreeNode;
+
+/**
+ * 124 二叉树中的最大路径和
+ *
+ * 二叉树中的 路径 被定义为一条节点序列，序列中每对相邻节点之间都存在一条边。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个
+ * 节点，且不一定经过根节点。
+ * 
+ * 路径和 是路径中各节点值的总和。
+ * 
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * 
+ * 示例 1：
+ * 
+ * <img src="https://assets.leetcode.com/uploads/2020/10/13/exx1.jpg"/>
+ * 
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ * 示例 2：
+ * 
+ * <img src="https://assets.leetcode.com/uploads/2020/10/13/exx2.jpg"/>
+ * 
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ * 
+ * 提示：
+ * 
+ * 树中节点数目范围是 [1, 3 * 104]
+ * -1000 <= Node.val <= 1000
+ */
+
+public class 二叉树中的最大路径和 {
+
+    int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+
+    public int maxGain(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        // 递归计算左右子节点的最大贡献值
+        // 只有在最大贡献值大于 0 时，才会选取对应子节点
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+
+        // 节点的最大路径和取决于该节点的值与该节点的左右子节点的最大贡献值
+        int priceNewpath = node.val + leftGain + rightGain;
+
+        // 更新答案
+        maxSum = Math.max(maxSum, priceNewpath);
+
+        // 返回节点的最大贡献值
+        return node.val + Math.max(leftGain, rightGain);
+    }
+
+    @Test
+    public void test() {
+        Object[] root = { -10, 9, 20, null, null, 15, 7 };
+        TreeNode node = TreeNode.arrToNode(root);
+        int ans = maxPathSum(node);
+        System.out.println(ans);
+
+    }
+}
