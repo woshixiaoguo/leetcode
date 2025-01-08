0d72016d919f02697492e715d97f4cf4367c6df2
Apollo <isguohr@qq.com>
2025-01-08 18:36:52 +0800

    面试经典150题-图

---
+++ src/main/java/interview_classic_150/岛屿数量.java
@@ -0,0 +1,91 @@
+package interview_classic_150;
+
+import org.junit.Test;
+
+/**
+ * 200 岛屿数量
+ *
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * 
+ * 此外，你可以假设该网格的四条边均被水包围。
+ * 
+ * 示例 1：
+ * 
+ * 输入：grid = [
+ * ["1","1","1","1","0"],
+ * ["1","1","0","1","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","0","0","0"]
+ * ]
+ * 输出：1
+ * 
+ * 示例 2：
+ * 
+ * 输入：grid = [
+ * ["1","1","0","0","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","1","0","0"],
+ * ["0","0","0","1","1"]
+ * ]
+ * 输出：3
+ * 
+ * 提示：
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] 的值为 '0' 或 '1'
+ */
+
+public class 岛屿数量 {
+
+    public int numIslands(char[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    ans++;
+                }
+            }
+
+        }
+        return ans;
+    }
+
+    public void dfs(char[][] grid, int r, int c) {
+        int h = grid.length;
+        int w = grid[0].length;
+        if (r < 0 || c < 0 || r >= h || c >= w || grid[r][c] == '0')
+            return;
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c + 1);
+    }
+
+    @Test
+    public void test() {
+        char[][] grid = {
+                { '1', '1', '1', '1', '0' },
+                { '1', '1', '0', '1', '0' },
+                { '1', '1', '0', '0', '0' },
+                { '0', '0', '0', '0', '0' }
+        };
+        char[][] grid2 = {
+                { '1', '1', '0', '0', '0' },
+                { '1', '1', '0', '0', '0' },
+                { '0', '0', '1', '0', '0' },
+                { '0', '0', '0', '1', '1' }
+        };
+        char[][] grid3 = {
+                { '1', '1', '1' }, { '0', '1', '0' }, { '1', '1', '1' }
+        };
+
+        int ans = numIslands(grid2);
+        System.out.println(ans);
+    }
+}
